0fab6bb029879a3c2cb92dc983eed642ee7c3be3
abdul <abdul2213@gmail.com>
2022-12-08 16:38:05 +0000

    Improving User Experiance of Hangman

--- SteppingStoneProjects/Hangman.py
+++ SteppingStoneProjects/Hangman.py
@@ -1,29 +1,43 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+from SteppingStoneProjects.Imports.hangman_words import word_list
+
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
- display += "_"
-print(display)
-lives = 6
-game_over = False
-while not game_over:
+    display += "_"
+
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-    if letter != guess:
-        lives -= 1
-    print(f"You have {lives} remaining")
 
-    print(display)
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
-        game_over = True
-        chosen_word2 = ''.join(display)
-        print(f"Player Won, the final work was {chosen_word2}")
+        end_of_game = True
+        print("You win.")
+
+    from SteppingStoneProjects.Imports.hangman_art import stages
+    print(stages[lives])
