937f795f5a1fd3734367088f26ece34ea8567c27
abdul <abdul2213@gmail.com>
2022-12-10 16:19:35 +0000

    finalising encoder/decoder

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import time
+# import sys
+# from time import sleep
+#
+# def typing_function():
+#  words = "This is just a test :P"
+#  for char in words:
+#     sleep(0.2)
+#     sys.stdout.write(char)]
+#
+# typing_function()
+# ^^^ function to make text appear as its being typed ^^^
 
-# name = input("What is your name?")
-# print("Hello " + name)
-# time.sleep(1)
-# print("Let's find when you'll die!!")
 
-import sys
-from time import sleep
 
-def typing_function():
- words = "This is just a test :P"
- for char in words:
-    sleep(0.2)
-    sys.stdout.write(char)]
 
-typing_function()
+
+
+
+
