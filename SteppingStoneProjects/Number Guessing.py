1e45f2a829e4ec5dd1489afb0189d374760da3d9
abdul <abdul2213@gmail.com>
2022-12-26 20:35:15 +0000

    Completed game

---
+++ SteppingStoneProjects/Number Guessing.py
@@ -0,0 +1,37 @@
+import random
+
+print("Welcome to the guessing game")
+print("I'm thinking of a number between 1 and 100")
+diffculty = input("Choose a diffculty between easy and hard?:")
+
+if diffculty == "easy":
+    lives = 10
+else:
+    lives = 5
+def guess(lives):
+    computer_guess = random.randint(1,101)
+    print(f"the number is {computer_guess}")
+    lives_left = lives
+    game_over = False
+    while not game_over:
+        print(f"You have {lives_left} lives left")
+        user_guess = int(input("Guess a number between 1 and 100: "))
+        if user_guess == computer_guess:
+            game_over = True
+            print("Well done, You have guessed correctly")
+        elif user_guess > computer_guess:
+            lives_left -= 1
+            print("Your guess is too high")
+        else:
+            lives_left -= 1
+            print("Your guess is too low")
+        if lives_left == 0:
+            game_over = True
+            print("You ran out lives, you lost the game")
+guess(lives)
+play_on = input("Would you like to play again? 'y' or 'n'?")
+
+if play_on == "y":
+    guess(lives)
+else:
+    print("Thank you for playing")
