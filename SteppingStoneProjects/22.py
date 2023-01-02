b9485187bed7a198f322adaa5a52e34fb1edea29
abdul <abdul2213@gmail.com>
2023-01-02 17:01:39 +0000

    Improvements

--- SteppingStoneProjects/22.py
+++ SteppingStoneProjects/22.py
@@ -11,7 +11,6 @@ class Card:
         # Return a string representation of the card
         return f"{self.value} of {self.suit}"
 
-
 # Deck class to represent the deck of cards
 class Deck:
     def __init__(self):
