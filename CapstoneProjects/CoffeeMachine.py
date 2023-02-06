73fcca9940546372ac93c6e1a90d1aabbe49ac6f
abdul <abdul2213@gmail.com>
2023-02-06 15:16:48 +0000

    completed game

--- CapstoneProjects/CoffeeMachine.py
+++ CapstoneProjects/CoffeeMachine.py
@@ -1,36 +1,6 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from CapstoneProjects.resources.ResourcesForCoffeeMachine import MENU, resources
 
 profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
 
 def is_resource_sufficient(order_ingredients):
     """Returns True when order can be made, False if ingredients are insufficient."""
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from CapstoneProjects.resources.ResourcesForCoffeeMachine import MENU
+from CapstoneProjects.resources.ResourcesForCoffeeMachine import MENU, resources
+from prettytable import PrettyTable
 
 
-
-
-
+table = PrettyTable()
 
 print(MENU)
+
+table.add_column("Beravage", ["espresso", "latte", "cappuccino"])
+table.add_column("Water", [50, 200, 250])
+table.add_column("Milk", [0, 150, 100])
+table.add_column("Coffee", [18, 24, 24])
+
+
+print(table)
+
 
