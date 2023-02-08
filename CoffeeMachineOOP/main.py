4acd9357ff2d19304b30351000b369c4c9190ff1
abdul <abdul2213@gmail.com>
2023-02-08 20:46:41 +0000

    completed game

--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -9,6 +9,7 @@ my_menu = Menu()
 
 is_on = True
 while is_on:
+    options = my_menu.get_items()
     command = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if command == "report":
         my_coffee_machine.report()
@@ -17,8 +18,11 @@ while is_on:
         is_on = False
     else:
         drink = my_menu.find_drink(command)
-        if my_coffee_machine.is_resource_sufficient(drink):
+        if my_coffee_machine.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+            my_coffee_machine.make_coffee(drink)
+        else:
+            print("Restarted")
 
-        print(a)
+
 
 
