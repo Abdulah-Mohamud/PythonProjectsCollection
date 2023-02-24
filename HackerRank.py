7a2117cbeece43a99ac8bbd0f8f7da27c2a7b745
abdul <abdul2213@gmail.com>
2023-02-24 11:16:48 +0000

    Completed Code

---
+++ HackerRank.py
@@ -0,0 +1,18 @@
+for i in range(1, int(input())+1):
+    print((10**i//9)**2)
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(1, n + 1):
+    if i == n:
+        for j in range(n):
+            print(n - j, end="")
+    else:
+        print(i, end="")
+
+t = divmod(input(), input())
+print(t[0])
+print(t[1])
+print(t)
